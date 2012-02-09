f655a6a6003a52118e7d3ce7be1933d17f0613aa
David Underhill <david@pocketgems.com>
2012-02-08 20:52:37 -0800

    use opponent win percentages to compute standings

--- swiss.py
+++ swiss.py
@@ -1,6 +1,8 @@
 """Computes swiss pairings"""
 import random
 
+GAME_POINTS_PER_BYE = 6
+STANDINGS = {}
 
 class PlayerStanding(object):
     """Info about a given player in the swiss tournament"""
@@ -8,15 +10,23 @@ class PlayerStanding(object):
         self.player = player
         self.match_points = 0
         self.game_points = 0
+        self.games_played = 0
         self.previous_opponents = []
+        STANDINGS[player] = self
 
     def record_match(self, other_player, game_wins, game_losses, game_draws):
         """Record a match result between this player and another player"""
+        if self.player == 'underhill':
+            import pdb; pdb.set_trace()
+
+        num_games = game_wins + game_losses + game_draws
         my_game_points = 3 * game_wins + game_draws
         self.game_points += my_game_points
+        self.games_played += num_games
 
         other_game_points = 3 * game_losses + game_draws
         other_player.game_points += other_game_points
+        other_player.games_played += num_games
 
         if my_game_points > other_game_points:
             self.match_points += 3
@@ -29,16 +39,64 @@ class PlayerStanding(object):
         self.previous_opponents.append(other_player)
         other_player.previous_opponents.append(self)
 
-    def record_bye(self, game_points_earned):
+    def record_bye(self, game_points_earned=GAME_POINTS_PER_BYE):
         """Record a BYE for this player (like a clean sweep win)"""
         self.game_points += game_points_earned
         self.match_points += 3
         self.previous_opponents.append('BYE')
 
+    def match_win_percentage(self, omit_bye=True):
+        """Return the percentage of matches won (minimum 0.33) excluding BYEs"""
+        n = float(len(self.previous_opponents))
+        mp = self.match_points
+        if omit_bye and 'BYE' in self.previous_opponents:
+            mp -= 3
+            n -= 1
+        return max(0.33, mp / (3 * n or -1))
+
+    def game_win_percentage(self, game_points_for_bye=GAME_POINTS_PER_BYE, omit_bye=True):
+        """Return the percentage of games won (minimum 0.33) excluding BYEs"""
+        n = float(self.games_played)
+        gp = self.game_points
+        if omit_bye and 'BYE' in self.previous_opponents:
+            gp -= game_points_for_bye
+        return max(0.33, gp / (3 * n or -1))
+
+    def opponent_match_win_percentage(self, standings=STANDINGS):
+        """Return the average match win percentage of opponents played"""
+        tot = 0
+        n = 0
+        for opp in self.get_opponents(standings):
+            n += 1
+            tot += opp.match_win_percentage()
+        return tot / (n or -1)
+
+    def opponent_game_win_percentage(self, standings=STANDINGS):
+        """Return the average game win percentage of opponents played"""
+        tot = 0
+        n = 0
+        for opp in self.get_opponents(standings):
+            n += 1
+            tot += opp.game_win_percentage()
+        return tot / (n or -1)
+
+    def get_opponents(self, standings=STANDINGS):
+        """Gets the PlayerStanding objects for players this player has played against"""
+        return [standings[player_or_bye(p)] for p in self.previous_opponents if p != 'BYE']
+
+    def standing_tuple(self):
+        """Returns a tuple which has metrics for evaluating rank in priority order"""
+        return (self.match_points,
+                self.opponent_match_win_percentage(),
+                self.game_win_percentage(),
+                self.opponent_game_win_percentage())
+
     def __cmp__(self, other):
         """Higher standings come first"""
-        return -cmp((self.match_points, self.game_points),
-                    (other.match_points, other.game_points))
+        ret = -cmp(self.standing_tuple(), other.standing_tuple())
+        if ret == 0:
+            return cmp(self.player, other.player)
+        return ret
 
     def __eq__(self, other):
         if not isinstance(other, PlayerStanding):
@@ -49,12 +107,25 @@ class PlayerStanding(object):
         return not self.__eq__(other)
 
     def __repr__(self):
-        fmt = '%s: MP=%d gp=%d prev_opponents=[%s]'
-        return fmt % (self.player, self.match_points,
+        fmt = '%010s    MP=%2d    OMW%%=%.2f    gp=%2d    OGW%%=%.2f  MW%%=%.2f  GW%%=%.2f  #GP=%2d  opp=[%s]'
+        prev_opp_str = ','.join(str(player_or_bye(p))
+                               for p in sorted(self.previous_opponents,
+                                               key=lambda p : player_or_bye(p)))
+        return fmt % (self.player,
+                      self.match_points,
+                      self.opponent_match_win_percentage(),
                       self.game_points,
-                      ','.join(str(p == 'BYE' and 'BYE' or p.player)
-                               for p in self.previous_opponents))
+                      self.opponent_game_win_percentage(),
+                      self.match_win_percentage(),
+                      self.game_win_percentage(),
+                      self.games_played,
+                      prev_opp_str)
 
+def player_or_bye(p):
+    """Return the player's name (or BYE if p is a BYE)"""
+    if p == 'BYE':
+        return p
+    return p.player
 
 def assign_pairings(player_standings):
     """Given the current standings, return a list of 2-tuples of who should play
