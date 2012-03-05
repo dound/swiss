61bba8752bfd39a1cfd74d80f4250b67581d908f
David Underhill <david@pocketgems.com>
2012-03-04 22:27:21 -0800

    update elo calc to support draws

--- elo.py
+++ elo.py
@@ -1,24 +1,38 @@
-"""
-Adapted from: http://forrst.com/posts/An_Elo_Rating_function_in_Python_written_for_foo-hQl#comment-481138
-"""
+"""Elo calculation"""
 import math
 
-def calculate_elo_rank(winner_rank, loser_rank, penalize_loser=True):
-    rank_diff = winner_rank - loser_rank
-    exp = (rank_diff * -1) / 400
-    odds = 1 / (1 + math.pow(10, exp))
-    if winner_rank < 2100:
-        k = 32
-    elif winner_rank >= 2100 and winner_rank < 2400:
-        k = 24
+
+def expected_score(rating_a, rating_b):
+    """Returns the expetected score for a game between the specified players"""
+    denom = 1 + math.pow(10, (rating_b - rating_a) / 400.)
+    return 1 / denom
+
+
+def get_k_factor(rating):
+    """Returns the k-factor for updating Elo.
+
+    Higher k-factor weights recent results more heavily.  The returned k-factor
+    is the one used by USCF.
+    """
+    if rating < 2100:
+        return 32
+    elif rating < 2400:
+        return 24
     else:
-        k = 16
-    new_winner_rank = int(round(winner_rank + (k * (1 - odds))))
-    if penalize_loser:
-        new_rank_diff = new_winner_rank - winner_rank
-        new_loser_rank = loser_rank - new_rank_diff
-    else:
-        new_loser_rank = loser_rank
-    if new_loser_rank < 1:
-        new_loser_rank = 1
-    return (new_winner_rank, new_loser_rank)
+        return 16
+
+
+def calculate_new_elos(rating_a, rating_b, score_a):
+    """Calculates and returns the new Elo ratings for two players.
+
+    score_a is 1 for a win by player A, 0 for a loss by player A, or 0.5 for a
+    draw.
+    """
+    e_a = expected_score(rating_a, rating_b)
+    e_b = 1 - e_a
+    a_k = get_k_factor(rating_a)
+    b_k = get_k_factor(rating_b)
+    new_rating_a = rating_a + a_k * (score_a - e_a)
+    score_b = 1.0 - score_a
+    new_rating_b = rating_b + b_k * (score_b - e_b)
+    return int(round(new_rating_a)), int(round(new_rating_b))
